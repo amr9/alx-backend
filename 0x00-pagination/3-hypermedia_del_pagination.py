4e55a891999455f0502b0840e1ad843c26bad844
amr9 <amr12201148@yahoo.com>
2024-08-31 11:51:32 -0700

    pep8 validation error

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """"""
+        """comments"""
         assert type(index) is int and type(page_size) is int
         assert 0 <= index < len(self.dataset())
         dataset = self.indexed_dataset()
